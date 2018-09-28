cfe947eae5e7c24762920704e54bf457959c3356
makiintoch <makiintoch@gmail.com>
2018-09-28 19:14:20 +0300

    add toolbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Hello vue</h1>
+  <v-app>
+    <v-navigation-drawer app temporary></v-navigation-drawer>
+
+    <v-toolbar app>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>AdBoard</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
