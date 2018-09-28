442389d5db259d4747e7c47e5132ebf06a109666
makiintoch <makiintoch@gmail.com>
2018-09-28 20:29:17 +0300

    add drawer

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <template>
   <v-app>
-    <v-navigation-drawer app temporary></v-navigation-drawer>
+    <v-navigation-drawer 
+      app 
+      temporary
+      v-model="drawer"
+    >
+      <v-list>
+        <v-list-tile
+          v-for="link of links"
+          :key="link.title"
+          :to="link.url"
+        >
+          <v-list-tile-action>
+            <v-icon>{{link.icon}}</v-icon>
+          </v-list-tile-action>
+
+          <v-list-tile-content>
+            <v-list-tile-title v-text="link.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-toolbar app>
-      <v-toolbar-side-icon></v-toolbar-side-icon>
-      <v-toolbar-title>AdBoard</v-toolbar-title>
+      <v-toolbar-side-icon 
+        @click="drawer = !drawer"
+        class="hidden-md-and-up"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <v-icon>widgets</v-icon> 
+        AdBoard
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat>Link One</v-btn>
+        <v-btn 
+          v-for="link in links"
+          :key="link.title"
+          :to="link.url"
+          flat
+        >
+          <v-icon left>{{link.icon}}</v-icon>
+          {{link.title}}
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <v-content>
@@ -18,5 +52,17 @@
 
 <script>
 export default {
+  data () {
+    return {
+      drawer: false,
+      links: [
+        {title: 'Login', icon: 'lock', url: '/login'},
+        {title: 'Registration', icon: 'face', url: '/registration'},
+        {title: 'Orders', icon: 'bookmark_border', url: '/orders'},
+        {title: 'New ad', icon: 'note_add', url: '/new'},
+        {title: 'My ads', icon: 'list', url: '/list'}
+      ]
+    }
+  }
 }
 </script>
