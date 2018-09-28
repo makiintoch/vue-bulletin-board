4267db91815239ad2f07014abe2994ba6a4f8b93
makiintoch <makiintoch@gmail.com>
2018-09-28 20:53:16 +0300

    add components

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,7 @@ export default {
         {title: 'Login', icon: 'lock', url: '/login'},
         {title: 'Registration', icon: 'face', url: '/registration'},
         {title: 'Orders', icon: 'bookmark_border', url: '/orders'},
-        {title: 'New ad', icon: 'note_add', url: '/new'},
+        {title: 'New ad', icon: 'note_add', url: '/create'},
         {title: 'My ads', icon: 'list', url: '/list'}
       ]
     }
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,52 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from '@/components/Home'
+import Ad from '@/components/Ads/Ad'
+import List from '@/components/Ads/List'
+import Create from '@/components/Ads/Create'
+import Login from '@/components/Auth/Login'
+import Registration from '@/components/Auth/Registration'
+import Orders from '@/components/User/Orders'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: '',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/ad/:id',
+      name: 'ad',
+      component: Ad
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: List
+    },
+    {
+      path: '/create',
+      name: 'create',
+      component: Create
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/registration',
+      name: 'registration',
+      component: Registration
+    },
+    {
+      path: '/orders',
+      name: 'orders',
+      component: Orders
+    }
+  ],
+  mode: 'history'
 })
