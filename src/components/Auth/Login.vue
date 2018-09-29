a5c3304eaa7acc770f5ab6ac6b8dbc13fb1c3272
makiintoch <makiintoch@gmail.com>
2018-09-29 20:01:32 +0300

    create UI Login page

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,18 +1,78 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Login page</h1>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-content>
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md6>
+          <v-card class="elevation-10">
+            <v-toolbar dark color="primary">
+              <v-toolbar-title>Login form</v-toolbar-title>
+            </v-toolbar>
+            <v-card-text>
+              <v-form v-model="valid" ref="form" validation>
+                <v-text-field 
+                  prepend-icon="person" 
+                  name="email" 
+                  label="Email" 
+                  type="email"
+                  :rules="emailRules"
+                  v-model="email"
+                  required
+                ></v-text-field>
+                <v-text-field 
+                  id="password" 
+                  prepend-icon="lock" 
+                  name="password" 
+                  label="Password" 
+                  type="password"
+                  :rules="passwordRules"
+                  v-model="password"
+                  required
+                ></v-text-field>
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn 
+                color="primary"
+                :disabled="!valid"
+                @click="onSubmit"
+              >Login</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
 export default {
   data () {
     return {
+      valid: false,
+      email: '',
+      password: '',
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+/.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+      ]
+    }
+  },
 
+  methods: {
+    onSubmit () {
+      if (this.$refs.form.validate()) {
+        const user = {
+          email: this.email,
+          password: this.password
+        }
+
+        console.log(user)
+      }
     }
   }
 }
