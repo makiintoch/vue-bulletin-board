067727274ad6546a1474dcf30ff1e5a991876aae
makiintoch <makiintoch@gmail.com>
2018-10-02 14:51:56 +0300

    create UI List ad page

--- src/components/Ads/List.vue
+++ src/components/Ads/List.vue
@@ -1,8 +1,37 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Ad list page</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text-secondary mb-3">List ads</h1>
+
+        <v-card 
+          class="elevation-10 mb-3"
+          v-for="item in ads"
+          :key="item.id"
+        >
+          <v-layout row>
+            <v-flex xs4>
+              <v-img
+                :src="item.src"
+                height="100%"
+              ></v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">{{item.title}}</h2>
+
+                <p>{{item.description}}</p>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn 
+                  class="info"
+                  :to="`/ad/${item.id}`"
+                >Show</v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +41,29 @@
 export default {
   data () {
     return {
-
+      ads: [
+        {
+          id: '1',
+          title: 'First ad',
+          description: 'Hello i am description',
+          promo: false,
+          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
+        },
+        {
+          id: '2',
+          title: 'Second ad',
+          description: 'Hello i am description',
+          promo: true,
+          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
+        },
+        {
+          id: '3',
+          title: 'Third ad',
+          description: 'Hello i am description',
+          promo: true,
+          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
+        }
+      ]
     }
   }
 }
