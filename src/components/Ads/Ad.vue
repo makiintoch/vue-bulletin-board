7576908689d81a57f3dbe01f8260393e2d613d36
makiintoch <makiintoch@gmail.com>
2018-10-02 15:29:33 +0300

    create UI Ad page

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,21 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Ad page</h1>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+            height="300"
+          ></v-img>
+          <v-card-text>
+            <h1 class="text--primary">Lorem</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi illo ut nemo a qui nihil. Eius hic nam excepturi ad ducimus eveniet quia doloribus modi sequi! Officiis rerum illum unde.</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn>Edit</v-btn>
+            <v-btn class="primary">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
