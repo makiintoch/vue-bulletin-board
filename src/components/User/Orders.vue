07bc56bf6a62ce74b62ac250d54c2e1b23635e32
makiintoch <makiintoch@gmail.com>
2018-10-02 12:58:23 +0300

    create UI Orders page

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,38 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Orders page</h1>
+      <v-flex xs12 sm6 offset-sm3>
+        <h1 class="text--secondary mb-3">Orders</h1>
+        <v-list
+          subheader
+          two-line
+        >
+          <v-list-tile 
+            avatar
+            v-for="item in orders"
+            :key="item.id"
+          >
+            <v-list-tile-action>
+              <v-checkbox 
+              color="success"
+                :input-value="item.done"
+                :readonly="item.done"
+                @change="markDone(item)"
+              ></v-checkbox>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+              <v-list-tile-title>{{item.name}}</v-list-tile-title>
+              <v-list-tile-sub-title>{{item.phone}}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn 
+                :to="`/ad/${item.adId}`"
+                class="primary"
+              >Show</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -12,7 +42,20 @@
 export default {
   data () {
     return {
-
+      orders: [
+        {
+          id: '123',
+          name: 'Igor',
+          phone: '099 999-99-99',
+          adId: '123',
+          done: false
+        }
+      ]
+    }
+  },
+  methods: {
+    markDone (order) {
+      order.done = true
     }
   }
 }
